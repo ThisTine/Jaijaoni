1ead8aacea244b9d7606e90691845830af548600
fah <phurichayakhem1@gmail.com>
2023-05-01 00:07:15 +0700

    save cancel button

--- lib/screens/edit_profile.dart
+++ lib/screens/edit_profile.dart
@@ -1,11 +1,8 @@
-import 'dart:html';
-
 import 'package:flutter/material.dart';
-import 'package:go_router/go_router.dart';
 import 'package:jaijaoni/components/circle_avata.dart';
-// import 'package:flutter/src/widgets/framework.dart';
-// import 'package:flutter/src/widgets/placeholder.dart';
 import 'package:jaijaoni/components/custom_app_bar.dart';
+
+import '../config/theme/custom_color.g.dart';
 
 class EditProfile extends StatelessWidget {
   const EditProfile({super.key});
@@ -14,102 +11,154 @@ class EditProfile extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: customAppBarBuilder(context, text: "Edit", backButton: true),
-      body: Padding(
-        padding: const EdgeInsets.symmetric(vertical: 16),
-        child: Column(
-          children: [
-            circleAvata(radius: 70),
-            const SizedBox(height: 10),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text("Upload Profile",
-                    style: TextStyle(
-                        color: Theme.of(context).colorScheme.primary,
-                        fontWeight: FontWeight.bold,
-                        fontSize:
-                            Theme.of(context).textTheme.titleSmall!.fontSize)),
-                IconButton(
-                    onPressed: () {
-                      // showModalBottomSheet(
-                      //     context: context,
-                      //     builder: (context) => Padding(
-                      //         padding: const EdgeInsets.all(20.0),
-                      //         child: ListView(children: [Row(children: [])])));
-                    },
-                    icon: Icon(Icons.edit,
-                        color: Theme.of(context).colorScheme.primary,
-                        size: Theme.of(context).textTheme.titleSmall!.fontSize))
-              ],
-            ),
-            const SizedBox(height: 20),
-            Divider(
-              color: Theme.of(context).colorScheme.primary,
-              thickness: 1,
-            ),
-            const SizedBox(height: 20),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 16),
-              child: Column(
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(vertical: 16),
+          child: Column(
+            children: [
+              circleAvata(radius: 70),
+              const SizedBox(height: 10),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      textlebel(context, "Name"),
-                      const SizedBox(width: 65),
-                      hintlebel(context, "Default Name"),
-                    ],
-                  ),
-                  const SizedBox(height: 20),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      textlebel(context, "Quote"),
-                      const SizedBox(width: 65),
-                      hintlebel(context, "add quote"),
-                    ],
-                  ),
-                  const SizedBox(height: 40),
+                  Text("Upload Profile",
+                      style: TextStyle(
+                          color: Theme.of(context).colorScheme.primary,
+                          fontWeight: FontWeight.bold,
+                          fontSize: Theme.of(context)
+                              .textTheme
+                              .titleSmall!
+                              .fontSize)),
+                  IconButton(
+                      onPressed: () {
+                        // showModalBottomSheet(
+                        //     context: context,
+                        //     builder: (context) => Padding(
+                        //         padding: const EdgeInsets.all(20.0),
+                        //         child: ListView(children: [Row(children: [])])));
+                      },
+                      icon: Icon(Icons.edit,
+                          color: Theme.of(context).colorScheme.primary,
+                          size:
+                              Theme.of(context).textTheme.titleSmall!.fontSize))
                 ],
               ),
-            ),
-            Divider(
-              color: Theme.of(context).colorScheme.primary,
-              thickness: 1,
-            ),
-            const SizedBox(height: 20),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 16),
-              child: Column(
-                children: [
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      textlebel(context, "Password"),
-                      const SizedBox(width: 45),
-                      hintlebel(context, "current password"),
-                    ],
-                  ),
-                  const SizedBox(height: 20),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      const SizedBox(width: 115),
-                      hintlebel(context, "new password"),
-                    ],
-                  ),
-                  const SizedBox(height: 20),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      const SizedBox(width: 115),
-                      hintlebel(context, "confirm password"),
-                    ],
-                  ),
-                ],
+              const SizedBox(height: 20),
+              Divider(
+                color: Theme.of(context).colorScheme.primary,
+                thickness: 1,
               ),
-            ),
-          ],
+              const SizedBox(height: 20),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 16),
+                child: Column(
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        textlebel(context, "Name"),
+                        const SizedBox(width: 65),
+                        hintlebel(context, "Default Name"),
+                      ],
+                    ),
+                    const SizedBox(height: 20),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        textlebel(context, "Quote"),
+                        const SizedBox(width: 65),
+                        hintlebel(context, "add quote"),
+                      ],
+                    ),
+                    const SizedBox(height: 40),
+                  ],
+                ),
+              ),
+              Divider(
+                color: Theme.of(context).colorScheme.primary,
+                thickness: 1,
+              ),
+              const SizedBox(height: 20),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 16),
+                child: Column(
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        textlebel(context, "Password"),
+                        const SizedBox(width: 45),
+                        hintlebel(context, "current password"),
+                      ],
+                    ),
+                    const SizedBox(height: 20),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        const SizedBox(width: 115),
+                        hintlebel(context, "new password"),
+                      ],
+                    ),
+                    const SizedBox(height: 20),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        const SizedBox(width: 115),
+                        hintlebel(context, "confirm password"),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+              const SizedBox(height: 15),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Row(
+                  children: [
+                    Expanded(
+                      child: TextButton(
+                          style: ButtonStyle(
+                              side: MaterialStateProperty.all<BorderSide>(
+                                  BorderSide(
+                                      color:
+                                          Theme.of(context).colorScheme.outline,
+                                      width: 1))),
+                          onPressed: () {},
+                          child: Text("Cancel",
+                              style: TextStyle(
+                                  color: Theme.of(context)
+                                      .extension<CustomColors>()!
+                                      .sourceRedbar,
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: Theme.of(context)
+                                      .textTheme
+                                      .titleSmall!
+                                      .fontSize))),
+                    ),
+                    const SizedBox(width: 10),
+                    Expanded(
+                      child: TextButton(
+                        style: ButtonStyle(
+                            backgroundColor: MaterialStateProperty.all<Color>(
+                                Theme.of(context).colorScheme.primary)),
+                        onPressed: () {},
+                        child: Text(
+                          "Save",
+                          style: TextStyle(
+                              color: Theme.of(context).colorScheme.onError,
+                              fontWeight: FontWeight.bold,
+                              fontSize: Theme.of(context)
+                                  .textTheme
+                                  .titleSmall!
+                                  .fontSize),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
