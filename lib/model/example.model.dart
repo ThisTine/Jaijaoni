f70821c0b56ebf8ed381706e3da93e05160aa051
I_AM_TINE <sutichok46@gmail.com>
2023-05-04 03:22:01 +0700

    [FIX] example model

--- lib/model/example.model.dart
+++ lib/model/example.model.dart
@@ -4,11 +4,22 @@ class User {
   final String id;
   final String name;
   final String? quote;
+  final List<String> friendList;
 
-  const User({required this.id, required this.name, this.quote});
+  const User(
+      {required this.id,
+      required this.name,
+      this.quote,
+      this.friendList = const []});
 
-  factory User.fromFireStore(DocumentSnapshot doc) {
-    Map data = doc.data as Map;
-    return User(id: doc.id, name: data['name'] ?? '', quote: data['quote']);
+  factory User.fromFireStore(DocumentSnapshot<Map<String, dynamic>> doc) {
+    Map<String, dynamic> data = doc.data()!;
+    return User(
+        id: doc.id,
+        name: data['name'] ?? '',
+        quote: data['quote'],
+        friendList: (data['friendList'] as List<dynamic>)
+            .map((e) => e.toString())
+            .toList());
   }
 }
